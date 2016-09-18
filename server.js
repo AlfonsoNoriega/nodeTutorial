f9c80991b9bbb29b8ebae048cdbc34db59da511c
Alfonso Noriega <anorviz@gmail.com>
2016-09-18 20:04:20 +0200

    Starting from the scratch

--- server.js
+++ server.js
@@ -1,15 +1,3 @@
 /**
  * Created by alfonsonoriega on 29/5/16.
  */
-var http = require("http");
-
-function onRequest(request, response)
-{
-    console.log("Request received.");
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
-    response.end();
-}
-
-http.createServer(onRequest).listen(8888);
-console.log("Server has started.");
