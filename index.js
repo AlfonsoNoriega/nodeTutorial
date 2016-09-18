8153158efdc3f41a9cf529374c641aea1a9f8987
Alfonso Noriega <anorviz@gmail.com>
2016-09-18 21:03:34 +0200

    We have create a router.js which will help us to deal with diferente actions for our web page. We have moified index.js and server.js in order to match this changes

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
  * Created by alfonsonoriega on 18/9/16.
  */
 var server = require("./server");
+var router = require("./router");
 
-server.start();
+server.start(router.route);
---
+++ router.js
@@ -0,0 +1,10 @@
+/**
+ * Created by alfonsonoriega on 18/9/16.
+ */
+
+
+function route(pathname) {
+    console.log("About to route a request for " + pathname);
+}
+
+exports.route = route;
