a8fda3a7e2709a99f4d9b72d28dfdb8287ef7a61
Alfonso Noriega <anorviz@gmail.com>
2016-09-18 20:10:48 +0200

    "Hellow World" Server in node. The most basic stuff

--- server.js
+++ server.js
@@ -1,3 +1,12 @@
 /**
  * Created by alfonsonoriega on 29/5/16.
  */
+
+var http = require("http");
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+
+}).listen(8888);
